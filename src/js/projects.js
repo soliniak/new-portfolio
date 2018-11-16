371ed90b613b35bef223651deb0ecc367448ba84
Rafi <soliniakdlagier@gmail.com>
2018-11-16 10:32:23 +0100

    Section portfolio for mobile

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,24 +1,112 @@
-let projects = {
+const Projects = {
   yoga: {
     name: "yoga",
     live: "https://soliniak.github.io/examples/yoga/index.html",
     src: "https://github.com/soliniak/Yoga",
     img: "img/project_icons/yoga.jpg",
     webp: "img/project_icons/yoga.webp",
-    alt: "WWW - Weekly WebDev Challange #16 by Rafi",
+    alt: "WWW - Weekly WebDev Challenge #16",
+    order: "10",
     stack: {
-      html5: "true",
-      js: "true",
-      gulp: "true",
-      photoshop: "true",
-      npm: "true",
-      sass: "true",
-      react: "false"
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", ""],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
     }
   },
   zest: {
-    name: "zest"
+    name: "zest",
+    live: "https://soliniak.github.io/examples/zest/",
+    src: "https://github.com/soliniak/ZEST",
+    img: "img/project_icons/zest.jpg",
+    webp: "img/project_icons/zest.webp",
+    alt: "WWW - Weekly WebDev Challenge #12",
+    order: "20",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", ""],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  mogo: {
+    name: "mogo",
+    live: "https://soliniak.github.io/examples/mogo/",
+    src: "https://github.com/soliniak/mogo",
+    img: "img/project_icons/mogo.jpg",
+    webp: "img/project_icons/mogo.webp",
+    alt: "WWW - WebDev Challenge #35",
+    order: "30",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", ""],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  lorem: {
+    name: "lorem",
+    live: "https://soliniak.github.io/examples/landingpage/",
+    src: "https://github.com/soliniak/zapakujloremipsum",
+    img: "img/project_icons/zapakujlorem.jpg",
+    webp: "img/project_icons/zapakujlorem.webp",
+    alt: "WWW - zapakujloremipsum - zadanie rekrutacyjne",
+    order: "40",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["false", ""],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  typer: {
+    name: "typer",
+    live: "https://soliniak.github.io/examples/typer/",
+    src: "https://github.com/soliniak/Word-typing-counter",
+    img: "img/project_icons/typer.jpg",
+    webp: "img/project_icons/typer.webp",
+    alt: "APP - word typing counter",
+    order: "25",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["false", ""],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["false", '<i class="fab fa-react icon--stack"></i>']
+    }
+  },
+  weatherApp: {
+    name: "weatherApp",
+    live: "https://soliniak.github.io/Weather-App/",
+    src: "https://github.com/soliniak/Weather-App",
+    img: "img/project_icons/weather-app.jpg",
+    webp: "img/project_icons/weather-app.webp",
+    alt: "APP - REST API Weather App",
+    order: "1",
+    stack: {
+      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
+      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
+      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
+      photoshop: ["true", ""],
+      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
+      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
+      react: ["true", '<i class="fab fa-react icon--stack"></i>']
+    }
   }
 };
 
-export default projects;
+export default Projects;
--- src/js/script.js
+++ src/js/script.js
@@ -1,118 +1,9 @@
-let projects = {
-  yoga: {
-    name: "yoga",
-    live: "https://soliniak.github.io/examples/yoga/index.html",
-    src: "https://github.com/soliniak/Yoga",
-    img: "img/project_icons/yoga.jpg",
-    webp: "img/project_icons/yoga.webp",
-    alt: "WWW - Weekly WebDev Challenge #16",
-    order: "10",
-    stack: {
-      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
-      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
-      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
-      photoshop: ["true", ""],
-      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
-      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
-      react: ["false", '<i class="fab fa-react icon--stack"></i>']
-    }
-  },
-  zest: {
-    name: "zest",
-    live: "https://soliniak.github.io/examples/zest/",
-    src: "https://github.com/soliniak/ZEST",
-    img: "img/project_icons/zest.jpg",
-    webp: "img/project_icons/zest.webp",
-    alt: "WWW - Weekly WebDev Challenge #12",
-    order: "20",
-    stack: {
-      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
-      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
-      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
-      photoshop: ["true", ""],
-      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
-      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
-      react: ["false", '<i class="fab fa-react icon--stack"></i>']
-    }
-  },
-  mogo: {
-    name: "mogo",
-    live: "https://soliniak.github.io/examples/mogo/",
-    src: "https://github.com/soliniak/mogo",
-    img: "img/project_icons/mogo.jpg",
-    webp: "img/project_icons/mogo.webp",
-    alt: "WWW - WebDev Challenge #35",
-    order: "30",
-    stack: {
-      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
-      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
-      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
-      photoshop: ["true", ""],
-      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
-      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
-      react: ["false", '<i class="fab fa-react icon--stack"></i>']
-    }
-  },
-  lorem: {
-    name: "lorem",
-    live: "https://soliniak.github.io/examples/landingpage/",
-    src: "https://github.com/soliniak/zapakujloremipsum",
-    img: "img/project_icons/zapakujlorem.jpg",
-    webp: "img/project_icons/zapakujlorem.webp",
-    alt: "WWW - zapakujloremipsum - zadanie rekrutacyjne",
-    order: "40",
-    stack: {
-      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
-      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
-      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
-      photoshop: ["false", ""],
-      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
-      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
-      react: ["false", '<i class="fab fa-react icon--stack"></i>']
-    }
-  },
-  typer: {
-    name: "typer",
-    live: "https://soliniak.github.io/examples/typer/",
-    src: "https://github.com/soliniak/Word-typing-counter",
-    img: "img/project_icons/typer.jpg",
-    webp: "img/project_icons/typer.webp",
-    alt: "APP - word typing counter",
-    order: "25",
-    stack: {
-      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
-      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
-      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
-      photoshop: ["false", ""],
-      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
-      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
-      react: ["false", '<i class="fab fa-react icon--stack"></i>']
-    }
-  },
-  weatherApp: {
-    name: "weatherApp",
-    live: "https://soliniak.github.io/Weather-App/",
-    src: "https://github.com/soliniak/Weather-App",
-    img: "img/project_icons/weather-app.jpg",
-    webp: "img/project_icons/weather-app.webp",
-    alt: "APP - REST API Weather App",
-    order: "1",
-    stack: {
-      html5: ["true", '<i class="fab fa-html5 icon--stack"></i>'],
-      js: ["true", '<i class="fab fa-js icon--stack"></i>'],
-      gulp: ["true", '<i class="fab fa-gulp icon--stack"></i>'],
-      photoshop: ["true", ""],
-      npm: ["true", '<i class="fab fa-npm icon--stack"></i>'],
-      sass: ["true", '<i class="fab fa-sass icon--stack"></i>'],
-      react: ["true", '<i class="fab fa-react icon--stack"></i>']
-    }
-  }
-};
+import Projects from "/js/projects.js";
 
-Object.entries(projects).forEach(([key, value]) => {
+Object.entries(Projects).forEach(([project, pval]) => {
   let stackData = "";
-  if (typeof value.stack != "undefined") {
-    Object.entries(value.stack).forEach(([stack, sval]) => {
+  if (typeof pval.stack != "undefined") {
+    Object.entries(pval.stack).forEach(([stack, sval]) => {
       if (sval[0] == "true") {
         stackData += sval[1];
         return stackData;
@@ -123,28 +14,29 @@ Object.entries(projects).forEach(([key, value]) => {
   let article = `
   <div class="card__description">
     <div class="card__header">
-        <h2 class="card__title">${value.name}</h2>
+        <h2 class="card__title">${pval.name}</h2>
         <span>
-          <a href="${value.live}" class="btn--square btn--live"> live </a>
-          <a href="${value.src}" class="btn--square"> src </a>
+          <a href="${pval.live}" class="btn--square btn--live"> live </a>
+          <a href="${pval.src}" class="btn--square"> src </a>
         </span>
     </div>
     <p class="card__text">
-      ${value.alt}
+      ${pval.alt}
     </p>
     <div class="card__stack">
       ${stackData}
     </div>
   </div>
   <picture class="image__container">
-    <source type="image/webp" srcset="${value.webp}" class="card__image" />
-    <img src="${value.img}" alt="${value.alt}" class="card__image" />
-  </picture>		`;
+    <source type="image/webp" srcset="${pval.webp}" class="card__image" />
+    <img src="${pval.img}" alt="${pval.alt}" class="card__image" />
+  </picture>`;
+
   let art = document.createElement("article");
   art.classList.add("card");
-  art.style.order = value.order;
+  art.style.order = pval.order;
   art.innerHTML = article;
-  main.appendChild(art);
+  portfolio.appendChild(art);
 });
 
 const cards = document.querySelectorAll(".card");
@@ -157,3 +49,32 @@ const cards = document.querySelectorAll(".card");
     card.style.backgroundColor = "#fff";
   });
 });
+
+// --- active section
+
+const btnAbout = document.querySelector(".btn--about");
+const btnPortfolio = document.querySelector(".btn--portfolio");
+const btnContact = document.querySelector(".btn--contact");
+
+btnAbout.addEventListener("click", () => {
+  activeSection(-100);
+  btnAbout.classList.add("link--active");
+});
+btnPortfolio.addEventListener("click", () => {
+  activeSection(0);
+  btnPortfolio.classList.add("link--active");
+});
+btnContact.addEventListener("click", () => {
+  activeSection(100);
+  btnContact.classList.add("link--active");
+});
+
+let activeSection = offset => {
+  about.style.left = -100 - offset + "vw";
+  portfolio.style.left = 0 - offset + "vw";
+  contact.style.left = 100 - offset + "vw";
+
+  btnAbout.classList.remove("link--active");
+  btnPortfolio.classList.remove("link--active");
+  btnContact.classList.remove("link--active");
+};
